1aa39fe66a3d092fad1fa9cb2b021c348dbea170
umbravi <umbravi@gmail.com>
2018-04-07 22:07:33 -0700

    Did dem diction.... I mean MAPS

---
+++ maps.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func maps() {
+	dictionary := make(map[string]int)
+
+	dictionary["key1"] = 7
+	dictionary["key2"] = 13
+
+	fmt.Println("map one:", dictionary)
+
+	valueOne := dictionary["key1"]
+	fmt.Println("value one:", valueOne)
+
+	fmt.Println("lenght of dictionary", len(dictionary))
+
+	delete(dictionary, "key2")
+	fmt.Println("map:", dictionary)
+
+	_, keyTwoPresent := dictionary["key2"]
+	fmt.Println("key two present:", keyTwoPresent)
+
+	declaredMap := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("declared map:", declaredMap)
+}
